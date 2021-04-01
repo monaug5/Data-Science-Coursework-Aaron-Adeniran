06ac1ff1317700b8ce012228c2309445fe710dae
monaug5 <aaronadeniran@aol.com>
2021-04-01 13:49:57 +0100

    Add files via upload

---
+++ import Task 1.py
@@ -0,0 +1,5 @@
+import random
+#Gathers the users name and asks the question "What is your name?"
+user_input = input("What is your name? ")
+number=random.randint(1,10)
+print(user_input, "Guess the number between 1 and 10")
